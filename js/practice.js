9fa04d6b46311152e6cf57c917d6a1307275ebf2
IcePad <haman_g@hotmail.com>
2016-09-30 10:00:04 +1300

    solved double up in page 1 bug!

--- js/practice.js
+++ js/practice.js
@@ -9,7 +9,7 @@ $(function () {
         //alert the user that they have clicked the button!
         alertify.success("Calculating Total Now!");
             //for loop to cycle through each page. (12 pages) 
-            for(var i = 0; i<=12; i++) {
+            for(var i = 1; i<=12; i++) {
             var pageNum = i;
             //Put JSON file in an object, based on what page number
             var shopifyJSON = 'http://shopicruit.myshopify.com/products.json?page=' + i;
@@ -19,20 +19,23 @@ $(function () {
             $.getJSON(shopifyJSON, function (json) {
                 var shopj = json;
                //For loop to cycle through each object in the JSON file.
-                for(var i = 0; i<Object.keys(shopj.products).length; i++){
+                for(var i = 0; i<(Object.keys(shopj.products)).length; i++){
                     //A constraint to check if product is a clock or a watch. 
                     if(shopj.products[i].product_type === "Clock" || shopj.products[i].product_type === "Watch"){
-                        console.log(shopj.products[i].variants[0].price);
-                        //Add the price of each individual clock/watch
-                        temp = temp + parseFloat(shopj.products[i].variants[0].price);
-                        //Prints out the total in a paragraph text in the html page, had it in the for loop to show a adding/tallying effect. 
-                        $("#totalValue").text("Total: $" +temp.toFixed(2));
+                        //Loop through each variant
+                        for(var j = 0;j<Object.keys(shopj.products[i].variants).length; j++){
+                            //Check how many variants for each clock and watch
+                            if(j === 0) console.log(shopj.products[i].title + " has " + Object.keys(shopj.products[i].variants).length + " different variants!");
+                            //console out individual title, variant number and price.
+                            console.log("PRICE for " + shopj.products[i].title + " variant " +  j + ": " + shopj.products[i].variants[j].price)
+                            //Add the price of each individual clock/watch
+                            temp = temp + parseFloat(shopj.products[i].variants[j].price);
+                            //display the total in a text container which displays on click. I kept the total in the for loop so it has a tallying affect!
+                            $("#totalValue").text("Total: $" +temp.toFixed(2));
+                        }
                     }
-                    
                 }
-                
             });
             }
-
     });
 });
