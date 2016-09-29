0dad5275e7b22ae70d8a12d9078bee2321e34ccd
IcePad <haman_g@hotmail.com>
2016-09-30 08:35:06 +1300

    Added comments to the jquery script!

--- js/practice.js
+++ js/practice.js
@@ -6,30 +6,32 @@ $(function () {
 
 
     $(".bb").click(function () {
+        //alert the user that they have clicked the button!
         alertify.success("Calculating Total Now!");
-
+            //for loop to cycle through each page. (12 pages) 
             for(var i = 0; i<=12; i++) {
-                var pageNum = i;
-            var weatherApi = 'http://shopicruit.myshopify.com/products.json?page=' + i;
-             var temp = 0;
-            //  var total = 0;
-            $.getJSON(weatherApi, function (json) {
-                console.log(json);
+            var pageNum = i;
+            //Put JSON file in an object, based on what page number
+            var shopifyJSON = 'http://shopicruit.myshopify.com/products.json?page=' + i;
+            //variable for temporary number
+            var temp = 0;
+            //function to getJSON oject from JSON file. 
+            $.getJSON(shopifySJON, function (json) {
                 var shopj = json;
-               
+               //For loop to cycle through each object in the JSON file.
                 for(var i = 0; i<Object.keys(shopj.products).length; i++){
-                     
+                    //A constraint to check if product is a clock or a watch. 
                     if(shopj.products[i].product_type === "Clock" || shopj.products[i].product_type === "Watch"){
                         console.log(shopj.products[i].variants[0].price);
-                         temp = temp + parseFloat(shopj.products[i].variants[0].price);
-                         //alert("Total: " +temp.toFixed(2));
-                         $("#totalValue").text("Total: $" +temp.toFixed(2));
+                        //Add the price of each individual clock/watch
+                        temp = temp + parseFloat(shopj.products[i].variants[0].price);
+                        //Prints out the total in a paragraph text in the html page, had it in the for loop to show a adding/tallying effect. 
+                        $("#totalValue").text("Total: $" +temp.toFixed(2));
                     }
                     
                 }
                 
             });
-            //  console.log("TOOOOOTAL: " + total);
             }
 
     });
